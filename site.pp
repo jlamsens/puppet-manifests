3cf86f5fd6bf507d3cd4c6e6d5be7797ece321fd
Jurgen Lamsens <jurgen.lamsens@telenet.be>
2013-06-29 22:26:42 +0200

    node declaration

--- site.pp
+++ site.pp
@@ -1,8 +1,3 @@
 # a "manifest" is Puppet's term for files containing configuration information
 # manifest files end in .pp
 # site.pp is the "main manifest file"
-
-# resource declaration
-file {'/tmp/hello':
-	content => "Hello world\n",
-}
